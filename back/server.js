85ff2d3a761f11b66c0919bd4309cf4dfcfb997f
MartinCaro29 <martin.caro784@gmail.com>
2024-12-13 23:54:11 +0100

    3rd change

--- back/server.js
+++ back/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const session = require('express-session')
 let path = require('path')
-const connectionString = "mongodb+srv://martincaro:origami2017@cluster0.mkcdz.mongodb.net/"
+const connectionString = process.env.MONGODB_URI
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
